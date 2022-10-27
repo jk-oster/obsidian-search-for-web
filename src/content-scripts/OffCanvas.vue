9b964c44e5be915175eb7a5194397abd2cfd7e03
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2022-10-27 04:10:50 +0200

    sizing and color

--- src/content-scripts/OffCanvas.vue
+++ src/content-scripts/OffCanvas.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="computedNotes?.length > 0 && searchString?.length > 2"
-    class="popup-container absolute right-5 top-10 dark">
+    class="popup-container absolute right-5 top-36 dark bg-gray-900 p-2 rounded">
     <button @click="searchInObsidianGui"
       class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Open
       Search in Obsidian</button>
