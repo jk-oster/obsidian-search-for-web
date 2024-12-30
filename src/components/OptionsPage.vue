6cd37707804b8d546e6e2ec1fb0db48247f52168
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2024-12-30 12:31:43 +0100

    update extension icons

--- src/components/OptionsPage.vue
+++ src/components/OptionsPage.vue
@@ -3,7 +3,7 @@
     <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
       <div class="container flex flex-wrap justify-between items-center mx-auto">
         <a href="#" class="flex items-center">
-          <img src="/icon/obsidian48.png" class="mr-3 h-6 sm:h-9" alt=""/>
+          <img src="/icon/icon48.png" class="mr-3 h-6 sm:h-9" alt=""/>
           <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Obsidian Web
                         Search</span>
         </a>
@@ -11,7 +11,7 @@
                 class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 aria-controls="navbar-default" aria-expanded="false">
           <span class="sr-only">Open main menu</span>
-          <img class="w-6 h-6" aria-hidden="true" src="/icon/obsidian48.png"/>
+          <img class="w-6 h-6" aria-hidden="true" src="/icon/icon48.png"/>
         </button>
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul
