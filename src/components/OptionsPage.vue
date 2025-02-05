bda4eb800547ce2f14635553356dedae4f5f6a67
Jakob O <jack98oster@gmail.com>
2025-02-05 17:37:26 +0100

    labels and tooltips improvement

--- src/components/OptionsPage.vue
+++ src/components/OptionsPage.vue
@@ -249,7 +249,7 @@
             </label>
             <select v-model="store.theme" @change="setTheme(store.theme)" id="theme" name="theme" required
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-              <option value="auto">Use Device Settings (default)</option>
+              <option value="auto">Auto (Default - Use Device Settings)</option>
               <option value="light">Light Mode</option>
               <option value="dark">Dark Mode</option>
             </select>
