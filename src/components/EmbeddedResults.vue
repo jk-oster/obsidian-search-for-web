2e3e910d06bde6d251da81094d37b17ab6e7b533
Jakob O <jack98oster@gmail.com>
2025-02-05 17:41:13 +0100

    ts fixes

--- src/components/EmbeddedResults.vue
+++ src/components/EmbeddedResults.vue
@@ -3,11 +3,11 @@ import {defineAsyncComponent, computed, ref} from 'vue'
 import {pageOptions} from "../config.js";
 
 const asyncComponents = {
-  loading: defineAsyncComponent(() => import('@/components/LoadingSpinner.vue')),
-  google: defineAsyncComponent(() => import('@/components/GoogleResults.vue')),
-  bing: defineAsyncComponent(() => import('@/components/BingResults.vue')),
-  kagi: defineAsyncComponent(() => import('@/components/KagiResults.vue')),
-  duckduckgo: defineAsyncComponent(() => import('@/components/DuckDuckGoResults.vue')),
+  loading: defineAsyncComponent(() => import('./LoadingSpinner.vue')),
+  google: defineAsyncComponent(() => import('./GoogleResults.vue')),
+  bing: defineAsyncComponent(() => import('./BingResults.vue')),
+  kagi: defineAsyncComponent(() => import('./KagiResults.vue')),
+  duckduckgo: defineAsyncComponent(() => import('./DuckDuckGoResults.vue')),
 }
 
 const componentToRender = ref<keyof typeof asyncComponents>('loading');
