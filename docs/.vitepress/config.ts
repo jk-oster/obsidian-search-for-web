467359de78be3bddd69ab5bb32c9f5ef33e47b6a
Jakob Osterberger <98315600+jk-oster@users.noreply.github.com>
2025-02-25 12:02:36 +0100

    [DOCS] config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -17,7 +17,7 @@ transformHead: ({ pageData }) => {
   },
 
   sitemap: {
-    hostname: 'https://jk-oster.github.io'
+    hostname: 'https://jk-oster.github.io/obsidian-search-for-web/''
   },
   
   // https://vitepress.dev/reference/default-theme-config
